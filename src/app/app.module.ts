ba2d9881c4beb3744737b73cff706f0e23d8ba34
ivancardozo11 <ivancardozo11@hotmail.com>
2018-11-15 20:27:49 -0300

    initializing repo project not working

---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { AgmCoreModule } from '@agm/core'; //maps
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Routes, RouterModule} from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LugaresService } from './services/lugares.service';
+import { CrearComponent } from './crear/crear.component';
+import { AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from '@angular/database';
+import {AngularFireAuthModule} from "angularfire2/auth";
+
+const appRoutes: Routes = [
+  {path:"", component: LugaresComponent},
+  {path:"lugares", component: LugaresComponent},
+  {path:"detalle/:id", component: DetalleComponent},
+  {path:"contacto", component: ContactoComponent},
+  {path:"crear", component: CrearComponent}
+];
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyDmIjBYqhpY1C84SPjvMv_fb4RN0QC8pIY",
+  authDomain: "square-222302.firebaseapp.com",
+  databaseURL: "https://square-222302.firebaseio.com",
+  storageBucket: "square-222302.appspot.com",
+  messagingSenderId: "489584955398"
+};
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResaltarDirective ,
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent,
+    ContactoComponent,
+    CrearComponent
+  ],
+  imports: [
+    BrowserModule,
+      FormsModule,
+      AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD13zATHhMoZlzi9FYJr17MSff79h5tlAg'
+    }),BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  providers: [LugaresService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/lugares/lugares.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { LugaresService } from '../services/lugares.service';
+
+@Component({
+  selector: 'app-lugares',
+  templateUrl: './lugares.component.html'
+})
+export class LugaresComponent {
+  title = 'Square';
+
+  lat:number = -34.6080556;
+  lng:number = -58.3724665;
+  lugares = null;
+  constructor(private lugaresService: LugaresService){
+    this.lugares = lugaresService.getLugares();
+
+  }
+}
