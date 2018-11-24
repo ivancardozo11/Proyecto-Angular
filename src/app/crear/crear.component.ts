e3b8ba3a875e00d5f710e20af5fd97e90ccdd155
ivancardozo11 <ivancardozo11@hotmail.com>
2018-11-24 19:50:10 -0300

    Type ahead implemented

--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { LugaresService } from '../services/lugares.service';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-crear',
@@ -9,14 +12,24 @@ import { ActivatedRoute } from '@angular/router';
 export class CrearComponent {
 lugar:any = {};
   id:any = null;
-constructor(private lugaresService: LugaresService, private route: ActivatedRoute){
-  // this.id = this.route.snapshot.params['id'];
-  // if(this.id != 'new'){
-  //   this.lugaresService.getLugar(this.id)
-  //   .subscribe(lugar =>{
-  //       this.lugar = lugar;
-  //   });
-  // }
+  // results es un string de resultados que me va a regresar google.
+  results$ : Observable<any>;
+  private searchField: FormControl;
+constructor(private lugaresService: LugaresService, private route: ActivatedRoute, private httpClient :HttpClient){
+  this.id = this.route.snapshot.params['id'];
+  if(this.id != 'new'){
+    this.lugaresService.getLugar(this.id)
+    .subscribe(lugar =>{
+        this.lugar = lugar;
+    });
+  }
+  const URL = 'https://maps.google.com/maps/api/geocode/json';
+  // Este metodos nos va a ayudar a saber en que momento se estan cambiando
+  this.searchField = new FormControl();
+  this.results$= this.searchField.valueChanges
+  .switchMap(query => this.httpClient.get(`${URL}?address=${query}`))
+  .map(response => response.json())
+  .map(response => response.results);
 }
  guardarLugar(lugar){
    var direccion = this.lugar.calle+','+this.lugar.ciudad+','+this.lugar.pais;
